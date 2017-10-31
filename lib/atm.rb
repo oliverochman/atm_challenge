2665a8a59f0d632cdaa2efb03bfe2fd4b0f899e6
oliver_ochman <oliver.ochman@hotmail.com>
2017-10-31 11:02:50 +0100

    adding card_expired? method

--- lib/atm.rb
+++ lib/atm.rb
@@ -12,9 +12,11 @@ class Atm
     when insufficient_funds_in_account?(amount, account)
       { status: false, message: 'insufficient funds', date: Date.today }
     when insufficient_funds_in_atm?(amount)
-  { status: false, message: 'insufficient funds in ATM', date: Date.today }
+      { status: false, message: 'insufficient funds in ATM', date: Date.today }
     when incorrect_pin?(pin_code, account.pin_code)
-  { status: false, message: 'wrong pin', date: Date.today }
+      { status: false, message: 'wrong pin', date: Date.today }
+    when card_expired?(account.exp_date)
+      { status: false, message: 'card expired', date: Date.today }
     else
       perform_transaction(amount, account)
     end
@@ -38,4 +40,8 @@ end
 
 def incorrect_pin?(pin_code, actual_pin)
   pin_code != actual_pin
+end
+
+def card_expired?(exp_date)
+  Date.strptime(exp_date, '%m/%y') < Date.today
 end
