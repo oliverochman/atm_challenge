c1f2aeb64521382555abaeb843855f574c90f77b
oliver_ochman <oliver.ochman@hotmail.com>
2017-11-01 13:02:03 +0100

    WIP account class - added exp_date & account_status methods

--- lib/account.rb
+++ lib/account.rb
@@ -1,14 +1,20 @@
 class Account
-  attr_accessor :pin_code, :balance
+  attr_accessor :pin_code, :balance, :exp_date, :account_status
 
   def initialize
     @pin_code = genererate_pin
     @balance = 0
+    @account_status = :active
+    @exp_date = future_date
   end
 
   private
 
   def genererate_pin
     rand(1000..9999)
+  end
+
+  def future_date
+    Date.today.next_year(5).strftime("%m/%y")
   end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,4 +1,5 @@
 require './lib/account.rb'
+require 'date'
 
 describe Account do
   it 'is expected to have a 4 digit pin number on initialize' do
@@ -8,5 +9,14 @@ describe Account do
 
   it 'is expected to have a balance of 0 on initialize' do
     expect(subject.balance).to eq 0
+  end
+
+  it 'is expected to have a active account status' do
+    expect(subject.account_status).to eq :active
+  end
+
+  it 'is expected to have an expiry date on initialize' do
+      expected_date = Date.today.next_year(5).strftime("%m/%y")
+      expect(subject.exp_date).to eq expected_date
   end
 end
