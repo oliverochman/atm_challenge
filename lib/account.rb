34072b82ba4b3bf8ead403e2715154f61ec93005
oliver_ochman <oliver.ochman@hotmail.com>
2017-11-01 12:02:58 +0100

    WIP Account class - pin generator added

--- lib/account.rb
+++ lib/account.rb
@@ -1,3 +1,13 @@
 class Account
+  attr_accessor :pin_code
 
+  def initialize
+    @pin_code = genererate_pin
+  end
+
+  private
+
+  def genererate_pin
+    rand(1000..9999)
+  end
 end
--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,5 +1,8 @@
 require './lib/account.rb'
 
 describe Account do
-
+  it 'is expected to have a 4 digit pin number on initialize' do
+    pin_lenght = Math.log10(subject.pin_code).to_i + 1
+    expect(pin_lenght).to eq 4
+  end
 end
