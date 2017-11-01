33a0f083fcd57fd6398cea75650874d69cfef88a
oliver_ochman <oliver.ochman@hotmail.com>
2017-11-01 15:15:49 +0100

    Finished account class - added method for person to own account and raise error if no owner set

--- lib/account.rb
+++ lib/account.rb
@@ -1,13 +1,14 @@
 class Account
-  attr_accessor :pin_code, :balance, :exp_date, :account_status
+  attr_accessor :pin_code, :balance, :exp_date, :owner, :account_status
 
   STANDARD_VALIDITY_YRS = 5
 
-  def initialize
+  def initialize(attrs = {})
     @pin_code = genererate_pin
     @balance = 0
     @account_status = :active
     @exp_date = set_expire_date
+    set_owner(attrs[:owner])
   end
 
   def deactivate
@@ -15,6 +16,14 @@ class Account
   end
 
   private
+
+  def set_owner(obj)
+    obj == nil ? missing_owner : @owner = obj
+  end
+
+  def missing_owner
+    raise 'An Account owner is required'
+  end
 
   def genererate_pin
     rand(1000..9999)
